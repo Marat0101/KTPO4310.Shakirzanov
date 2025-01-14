bd3d140f756ff1071a812dfeaa6d5cd4be20a359
Marat0101 <msakirzanov112@gmail.com>
2025-01-15 00:12:42 +0300

    Добавьте файлы проекта.

---
+++ KTPO4310.Shakirzanov.Lib/src/LogAn/LogAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KTPO4310.Shakirzanov.Lib.src.LogAn
+{
+    public class LogAnalyzer
+    {
+        
+        public bool WasLastFileNameValid { get; set; }
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("имя файла должно быть задано");
+            }
+            if (fileName.EndsWith(".log", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            
+            WasLastFileNameValid = true;
+            return true;
+        }
+    }
+}
---
+++ KTPO4310.Shakirzanov.UnitTest/src/LogAn/LogAnalyzerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using KTPO4310.Shakirzanov.Lib.src.LogAn;
+using NUnit.Framework;
+
+namespace KTPO4310.Shakirzanov.UnitTest.src.LogAn
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtennsion_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            Assert.True(result);
+        }
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            // Подготовка ткста
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            // Воздействие на тестерируемый объект
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.LOG");
+
+            Assert.IsFalse(result);
+        }
+        
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            // Подготовка ткста
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            // Воздействие на тестерируемый объект
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.log");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestCase("filewithgoodextension.LOG")]
+        [TestCase("filewithgoodextension.log")]
+        public void IsVaLidLogFileName_ValidExtension_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("file");
+           
+            Assert.True(result);
+            
+        }
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throws()
+        {
+            // Подготовка текста
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            // Воздействие на тестируемый объект
+            var ex = Assert.Catch<Exception>(() => analyzer.IsValidLogFileName(""));
+
+
+
+            StringAssert.Contains("имя файла должно быть задано", ex.Message);
+        }
+        [TestCase("goodfile.log",false)]
+        [TestCase("badfile.foo",true)]
+        
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            analyzer.IsValidLogFileName(file);
+            Assert.AreEqual(expected, analyzer.WasLastFileNameValid);
+        }
+    }
+
+}
+
